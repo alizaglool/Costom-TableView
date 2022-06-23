3f3e5693de3598fa83440c91040f973a861f41d8
Ali Zaglool <alizaglool90@gmail.com>
2022-06-23 17:44:46 +0200

    Initial Commit

---
+++ CostomTableViewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CostomTableViewTest
+//
+//  Created by A_Z on 6/23/22.
+//  Copyright © 2022 AliZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
